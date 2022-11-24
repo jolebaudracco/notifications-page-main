28976f324ad681c977f6c582e956cee2e1b2de08
Jole <yole126@gmail.com>
2022-11-24 18:34:16 -0300

    first commit

---
+++ data.js
@@ -0,0 +1,21 @@
+const notifications = [
+  {
+    username: 'Mark Webber',
+    title: 'reacted to your recent post My first tournament today!',
+    viewed: true
+  },
+  {
+    username: 'Angela Gray',
+    title : 'followed you',
+    viewed : true
+  },
+  {
+    username : 'Jacob Thompson',
+    title: 'has joined your group Chess Club',
+    viewed:false
+  },
+  {
+    username : 'Rizky' ,
+
+  }
+]
